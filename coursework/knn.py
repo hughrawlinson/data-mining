1087488e0b1a8b875d1a5f4c6e54b26ba62dc1d9
Hugh Rawlinson <hugh@codeoclock.net>
2015-11-30 06:19:09 +0000

    Numpy Everything
    Converts most structures to numpy, which makes everything fast enough to not need memoization. Generalizes most of the functions and greatly reduces duplicate code. There's probably more I could do on this front.

--- coursework/knn.py
+++ coursework/knn.py
@@ -1,65 +1,81 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+"""A module for building K-Nearest-Neighbor"""
 
 from sys import argv
 import numpy as np
 import csv
 from hashlib import md5
-import pickle
-
-
-distanceMatrices = {}
 
 
 def getDataAsMultidimensionalArrayFromFilename(filename):
+    """
+    Takes a path to a file, performs I/O, returns data as a multidimensional
+    array.
+
+    Named Keywords
+    filename -- a string containing the file path
+    """
     data = []
     header = []
     with open(filename, 'rb') as csvfile:
         datareader = csv.reader(csvfile, delimiter=',', quotechar='|')
         for row in datareader:
-            if(header == []):
+            if(len(header) < 1):
                 header = row
             else:
                 data.append(row)
-    return header, data
+    return header, np.array(data)
 
 
 def getMd5(arg):
+    """
+    Takes arbitrary data, returns an md5 hash
+    """
     x = md5()
     x.update(arg)
     return x.digest()
 
 
 def distanceBetweenTwoInstances(instanceA, instanceB):
+    """
+    Calculates the distance between two rows of our dataset
+    """
     distance = 0
-    if(len(instanceA) == len(instanceB)):
-        for i in range(0, len(instanceA)):
+    if(instanceA.size == instanceB.size):
+        for i in range(0, instanceA.size):
             if(instanceA[i] != instanceB[i]):
                 distance += 1
         return distance
     else:
-        syntaxErrorLol
+        print "Malformed Data"
+        exit(1)
 
 
-def buildDistanceMatrix(data):
-    try:
-        return distanceMatrices[getMd5(pickle.dumps(data))]
-    except Exception as e:
-        distanceMatrix = []
-        for i in data:
-            distanceRow = []
-            for j in data:
-                distanceRow.append(distanceBetweenTwoInstances(i, j))
-            distanceMatrix.append(i)
-        distanceMatrices[getMd5(pickle.dumps(data))] = distanceMatrix
-        return distanceMatrix
+def buildDistances(data, query):
+    """
+    Builds distances describing the distances between our
+    query and each of the rows in the dataset
+    """
+    distances = np.array([])
+    for i in data:
+        distance = distanceBetweenTwoInstances(i, query)
+        distances = np.append(distances, distance)
+    return distances
 
 
 def getNearestNeighbours(distanceRow, k):
-    arr = np.array(distanceRow)
-    return np.argpartition(arr, -k)[-k:]
+    """
+    Takes a row of distances, returns the indices of the
+    lowest k values
+    """
+    return np.argpartition(distanceRow, -k)[-k:]
 
 
 def getFeaturesFromNearestNeighbours(neighbourIndices, dataset, featureIndex):
+    """
+    Takes a list of row numbers, returns the set of their values
+    for a particular column in each row of our dataset.
+    """
     features = []
     for index in neighbourIndices:
         features.append(dataset[index][featureIndex])
@@ -67,6 +83,10 @@ def getFeaturesFromNearestNeighbours(neighbourIndices, dataset, featureIndex):
 
 
 def arrayToReverseDict(array):
+    """
+    Swaps keys and values from a list, in the process converting it into
+    a dict
+    """
     d = {}
     for index, value in enumerate(array):
         d[value] = index
@@ -74,47 +94,53 @@ def arrayToReverseDict(array):
 
 
 def vote(features):
-    hist = dict()
-    for index, feature in enumerate(features):
-        try:
-            hist[feature] += 1
-        except Exception as e:
-            hist[feature] = 1
+    """
+    Takes a list of features, returns the most common feature of the list.
+    """
+    hist = histogram(features)
     return sorted(hist, key=hist.get)[-1]
 
 
 def main(arg):
-    hist = {}
+    results = []
     k = int(arg[1])
     dataFilePath = arg[2]
     testDataFilePath = arg[3]
     searchKey = arg[4]
     header, data = getDataAsMultidimensionalArrayFromFilename(dataFilePath)
     _, testData = getDataAsMultidimensionalArrayFromFilename(testDataFilePath)
-    for index, queryInstance in enumerate(testData):
-        features, result = knn(
+    for queryRow in testData:
+        result = knn(
             k,
-            dataFilePath,
             searchKey,
-            queryInstance,
+            queryRow,
             header,
-            data)
-        try:
-            hist[result] += 1
-        except Exception as e:
-            hist[result] = 1
-    print hist
+            data
+        )
+        results.append(result)
+    print histogram(results)
 
 
-def knn(k, dataFilePath, searchKey, queryInstance, header, data):
+def knn(k, searchKey, queryRow, header, data):
     headerIndices = arrayToReverseDict(header)
-    distanceMatrix = buildDistanceMatrix(data)
-    neighbourIndices = getNearestNeighbours(queryInstance, k)
+    distances = buildDistances(data, queryRow)
+    neighbourIndices = getNearestNeighbours(distances, k)
     features = getFeaturesFromNearestNeighbours(
         neighbourIndices,
         data,
         headerIndices[searchKey])
-    return features, vote(features)
+    return vote(features)
+
+
+def histogram(data):
+    """Generates a histogram for arbitrary data"""
+    hist = {}
+    for index, value in enumerate(data):
+        try:
+            hist[value] += 1
+        except Exception:
+            hist[value] = 1
+    return hist
 
 
 if __name__ == '__main__':
